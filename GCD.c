9b135d1dbacf6dff356dd413a23f7c03ac20bc96
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2023-08-02 13:05:30 +0530

    Add files via upload

---
+++ GCD.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ int main(){
+    int num1,num2,temp;
+    printf("enter the first number:");
+    scanf("%d",&num1);
+    printf("enter the second number:");
+    scanf("%d",&num2);
+
+    while(num2!=0);{
+
+        temp=num2;
+        num2=num1%num2;
+        num1=temp;
+    }
+
+     printf("GCD of the number is %d",num1);
+
+    return 0;
+}
