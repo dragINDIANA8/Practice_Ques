1dfa54dd1fbaefad191d4eb076aa5e1872dc569e
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2024-07-18 00:06:44 +0530

    Add files via upload

---
+++ Element_present_in_Array.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Element_present_in_Array{
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,42,50};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the num:");
+        int num = sc.nextInt();
+        boolean isInArray = false;
+        
+        for(int element:arr){
+            if(num==element){
+                isInArray=true;
+                break;
+            }
+        }
+            if (isInArray) {
+                System.out.println("Yes the element is present in the array");
+                
+            }
+            else{
+                System.out.println("No the element is not present in the array");
+            }
+        
+        
+    }
+}
