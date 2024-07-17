f759d4317bc920913c0c6e5bb3103546600307c1
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2024-07-17 19:33:47 +0530

    Add files via upload

---
+++ Array.java.java
@@ -0,0 +1,16 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] arr = {100,50,60,40,30};
+
+        System.out.println("Array in actual order:");
+        for(int i=0; i< arr.length; i++){
+            System.out.println(arr[i]+" ");
+        }
+        System.out.println();
+        System.out.println("Arrya in reverse order: ");
+        for(int i= arr.length-1; i>=0; i--){
+            System.out.println(arr[i]+" ");
+        }
+    }
+    
+}
