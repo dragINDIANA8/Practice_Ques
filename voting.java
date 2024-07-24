41185fb452e920f2256124181d6fcd04a61510da
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2024-07-24 21:26:34 +0530

    Update and rename voting.c to voting.java

---
+++ voting.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class voting{
+public static void main(String[]args){
+    System.out.print("enter your age: ");
+    Scanner sc = new Scanner (System.in);
+        
+        int age = sc.nextInt();
+        if(age>=18){
+            System.out.print("You are eligible to vote");
+        }
+        else {
+            System.out.print("Sorry, you are not eligible to vote");
+        }
+    }
+}
