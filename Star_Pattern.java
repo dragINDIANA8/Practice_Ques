235c96b6bad2395d77031bc0c2f7f23bc1bbf8c5
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2024-08-01 21:16:05 +0530

    Update Star_Pattern.java

--- Star_Pattern.java
+++ Star_Pattern.java
@@ -5,7 +5,7 @@ class Star_Pattern {
            for(int j=0; j<i; j++){
                 System.out.print("*");
            }
-            System.out.println();
+            System.out.println(); 
        }
     }
 }
