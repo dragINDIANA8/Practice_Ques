c990e70e1005e1131cecda2c9433267a885213b2
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2024-08-06 21:32:28 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -4,7 +4,7 @@ class Codechef
 	public static void main (String[] args)
 	{
 		// your code goes here
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); // enter the no. you want to find the factorial of and then it will print the factorial of that no.
 		int n= sc.nextInt();
 		int fact = 1;
 		for(int i=1; i<=n;i++){
